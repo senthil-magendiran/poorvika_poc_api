dfb761ef15a4b8cdcb065c08eada7f76173bb9b7
senthil-magendiran <senthil.magendiran@softsuave.com>
2022-02-14 15:05:02 +0530

    mongoDB

--- src/utils/DBConnection.ts
+++ src/utils/DBConnection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connection = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/poorvika_poc")    
+        await mongoose.connect("mongodb+srv://senthil:FhcyHAY6L6hvrNt@cluster0.zugd4.mongodb.net/poorvika")    
         console.log("Data base connected");
     } catch (error: any) {
         console.log("Data base Connection Error", error.message);
