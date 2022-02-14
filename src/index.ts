099ffcb8a0d756c6be4e846a6048bfe1e03e1087
senthil-magendiran <senthil.magendiran@softsuave.com>
2022-02-14 18:23:33 +0530

    port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("Hiiiiiii...")
 })
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   DBConnection();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
